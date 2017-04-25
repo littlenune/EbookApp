b7310fcd57819cde4c0619ee272ae5e2d6bd27b9
Tunya <littlenune@gmail.com>
2017-04-25 11:53:34 +0700

    finish phase1

--- app/src/main/java/com/example/nune/ebookapp/MainActivity.java
+++ app/src/main/java/com/example/nune/ebookapp/MainActivity.java
@@ -1,21 +1,30 @@
 package com.example.nune.ebookapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.nune.ebookapp.model.Book;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private ArrayList<Book> books;
     private ArrayAdapter<Book> bookAdapter;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initListView();
-        loadTrendingTopics();
+        loadBooks();
+
     }
 
     private void initListView() {
@@ -32,13 +42,85 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,
                 books);
 
-        ListView listview = (ListView) findViewById(R.id.listView_topic_list);
+        ListView listview = (ListView) findViewById(R.id.listView_book_list);
         listview.setAdapter(bookAdapter);
     }
 
-    private void loadTrendingTopics() {
-        for ( int i = 0 ; i < 30 ; i++ ) {
-            bookAdapter.add(new Book("name",i+1));
+    private void loadBooks() {
+        BookFetcherTask task = new BookFetcherTask();
+        task.execute();
+    }
+
+    public void refreshBooks(View view) {
+        loadBooks();
+    }
+
+    public class BookFetcherTask extends AsyncTask<Void,Void,ArrayList<Book>> {
+
+        @Override
+        protected ArrayList<Book> doInBackground(Void... params) {
+            String bookListJsonStr = loadBookJson();
+
+            Log.d("none", "do in back" + bookListJsonStr);
+            if ( bookListJsonStr == null ){
+                Log.d("none","null book");
+                return null;
+            }
+
+            ArrayList<Book> results = new ArrayList<>();
+
+            try {
+                JSONArray jsonArray = new JSONArray(bookListJsonStr);
+
+                for ( int i = 0 ; i < jsonArray.length() ; i++ ){
+                    JSONObject bookJson = jsonArray.getJSONObject(i);
+                    Book book = new Book(bookJson.getString("title"),
+                            bookJson.getInt("id"),bookJson.getDouble("prize"),
+                            bookJson.getString("pub_year"),bookJson.getString("img_url"));
+                    results.add(book);
+                    Log.d("none", book.toString());
+                    Log.d("none" , "run test");
+                }
+            } catch (JSONException e){
+                Log.d("none", "catch");
+                return null;
+            }
+            return results;
+        }
+
+        private String loadBookJson() {
+            String result = "";
+            try {
+                URL bookUrl = new URL("https://theory.cpe.ku.ac.th/~jittat/courses/sw-spec/ebooks/books.json");
+                Log.d("none", "Load1");
+                URLConnection bookCon = bookUrl.openConnection();
+                Log.d("none", "Load2");
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        bookCon.getInputStream()));
+                Log.d("none", "Load3");
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    result += inputLine;
+
+                }
+                Log.d("none", "Load4");
+                return result;
+            } catch (IOException e) {
+                Log.d("none","exception loadBookJson");
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Book> results) {
+
+            if ( results != null) {
+                bookAdapter.clear();
+                for ( Book b : results ){
+                    bookAdapter.add(b);
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/nune/ebookapp/model/Book.java
+++ app/src/main/java/com/example/nune/ebookapp/model/Book.java
@@ -6,19 +6,48 @@ package com.example.nune.ebookapp.model;
 
 public class Book {
     private String title;
-    private int price;
+    private double price;
+    private String imageSrc;
+    private String year;
+    private int id;
 
-    public Book(String title, int price){
+
+    public Book(String title,int id, double price,String year, String imageSrc){
         this.title = title;
         this.price = price;
+        this.year = year;
+        this.id = id;
+        this.imageSrc = imageSrc;
     }
 
-    public int getPrice() { return price; }
+    public String getImageSrc() { return imageSrc; }
+    public void setImageSrc(String imageSrc) { this.imageSrc = imageSrc; }
+    public double getPrice() { return price; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
     public void setPrice(int price) { this.price = price; }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
     public String toString(){
-        return title + " " + price;
+        return title + " " + price + " " + imageSrc;
     }
 }
